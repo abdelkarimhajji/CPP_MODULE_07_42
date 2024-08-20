47883d7961adb801c12d7df4ea811b2e75771694
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-20 10:27:10 +0100

    add some attribute and constraction

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,17 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 09:40:58 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/20 09:45:37 by ahajji           ###   ########.fr       */
+/*   Created: 2024/08/20 09:46:25 by ahajji            #+#    #+#             */
+/*   Updated: 2024/08/20 10:26:21 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
 {
-    
-    return 0;
-}
+    private:
+        T* arr;
+        int size;
+    public:
+        Array();
+        Array(unsigned int n);
+};
+
+
+#endif
