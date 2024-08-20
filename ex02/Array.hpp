e4d66d3bfea7909316e330299e12cca2ef11dcae
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-20 15:01:56 +0100

    almost finish

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 09:46:25 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/20 11:55:43 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/20 15:00:36 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,51 @@ class Array
 {
     private:
         T* arr;
-        int size;
+        unsigned int _size;
     public:
-        Array() : arr(nullptr), size(0)
+        Array() : arr(nullptr), _size(0)
         {
             std::cout << "hi\n";
         }
-        Array(unsigned int n) : arr(new T[n]()), size(n)
+        Array(unsigned int n) : arr(new T[n]()), _size(n)
         {
             std::cout << "hi paramert\n" ;
+        }
+        Array(const Array& array) : arr(new T[array._size]), _size(array._size)
+        {
+            unsigned int i = 0;
+            while (i < _size)
+            {
+                arr[i] = array.arr[i];
+                i++;
+            }   
+        }
+        Array& operator=(const Array& array)
+        {
+            if(this != &array)
+            {
+                delete[] arr;
+                arr = new T[array._size];
+                _size = array._size;
+                unsigned int i = 0;
+                while (i < _size)
+                {
+                    arr[i] = array.arr[i];
+                    i++;
+                }   
+            }
+            return *this;
+        }
+        
+        T& operator[](unsigned int index)
+        {
+            if(index < 0 || index >= _size)
+                throw std::out_of_range("Array index out of range");
+            return arr[index];
+        }
+
+        unsigned int size() const {
+            return _size;
         }
 
         ~Array() {
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,17 +6,37 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 09:40:58 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/20 12:05:20 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/20 15:01:31 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+
+int getConst() {
+    return 10;
+}
 
 int main()
 {
     Array<int> arr(5);
     int * a = new int();
     std::cout << *a << std::endl;
+    
+    try
+    {
+        arr[1] = 8;
+        arr[9] = 11;
+        std::cout << arr[1] << std::endl;
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "this is the size : " << arr.size() << std::endl;
+    
+    
     delete a;
+
+    // std::cout << getConst() << "\n"
     return 0;
 }
