264379823a500c3a547e41979bd1d2baf5a60362
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-21 11:17:34 +0100

    i remove and update some examples

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:09:53 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/19 13:29:03 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/21 11:16:49 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int main()
     int array[] = {1, 2, 3, 4, 5};
     iter(array, 5, increment);
 
-    // Print the array to verify the elements have been incremented
     for (int i = 0; i < 5; ++i) {
         std::cout << array[i] << ' ';
     }
